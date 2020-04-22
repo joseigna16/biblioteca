14df26f1ca79b831372c974a51643d57009a9964
joseigna16 <poquenacho@gmail.com>
2020-04-22 18:29:38 -0500

    commit inicial

--- server/server.js
+++ server/server.js
@@ -5,6 +5,24 @@ const express = require('express');
 //Declarando constante app
 const app = express();
 const bodyParser = require('body-parser');
+
+
+
+//Habilitacion de CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Request-With, Content-Type,Accept, Authorization, token'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET', 'POST', 'PUT', 'DELETE', 'OPTIONS '
+    );
+    next();
+})
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({}));
 
